9501b19de8b0ff713378a69fe26e1446d6049e70
Emi2003187 <144975255+Emi2003187@users.noreply.github.com>
2025-07-23 22:18:59 -0700

    fix receta preview url

---
+++ consultorio_API/views_recetas.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseForbidden
+from django.views.generic import DetailView
+
+from .models import Receta
+
+
+class RecetaPreviewView(LoginRequiredMixin, DetailView):
+    """Previsualización simple de una receta para mostrar en un modal."""
+
+    model = Receta
+    template_name = "PAGES/recetas/_preview.html"
+    context_object_name = "receta"
+    pk_url_kwarg = "receta_id"
+
+    def dispatch(self, request, *args, **kwargs):
+        receta = self.get_object()
+        if not request.user.has_perm("consultorio.view_receta"):
+            return HttpResponseForbidden()
+        return super().dispatch(request, *args, **kwargs)
