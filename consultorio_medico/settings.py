5b47fc50a28548c812bbcf66d47ce56386c303ad
Emi2003187 <144975255+Emi2003187@users.noreply.github.com>
2025-07-25 20:56:36 -0700

    register icons template library

--- consultorio_medico/settings.py
+++ consultorio_medico/settings.py
@@ -70,6 +70,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'icons': 'consultorio_API.templatetags.icons',
+            },
         },
     },
 ]
