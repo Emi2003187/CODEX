287790fe645e484d58bc8962c72794c33b105bd3
Emi2003187 <144975255+Emi2003187@users.noreply.github.com>
2025-11-24 21:23:04 -0800

    Handle existing clave column in migration

--- consultorio_API/migrations/0002_medicamentocatalogo_clave.py
+++ consultorio_API/migrations/0002_medicamentocatalogo_clave.py
@@ -1,6 +1,22 @@
 from django.db import migrations, models
 
 
+def add_clave_column(apps, schema_editor):
+    MedicamentoCatalogo = apps.get_model("consultorio_API", "MedicamentoCatalogo")
+    table_name = MedicamentoCatalogo._meta.db_table
+    with schema_editor.connection.cursor() as cursor:
+        existing_columns = {
+            column.name for column in schema_editor.connection.introspection.get_table_description(cursor, table_name)
+        }
+
+    if "clave" in existing_columns:
+        return
+
+    field = models.CharField(max_length=100, null=True, blank=True)
+    field.set_attributes_from_name("clave")
+    schema_editor.add_field(MedicamentoCatalogo, field)
+
+
 def copiar_codigo(apps, schema_editor):
     MedicamentoCatalogo = apps.get_model("consultorio_API", "MedicamentoCatalogo")
     for med in MedicamentoCatalogo.objects.all():
@@ -15,10 +31,17 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="medicamentocatalogo",
-            name="clave",
-            field=models.CharField(blank=True, max_length=100, null=True),
+        migrations.SeparateDatabaseAndState(
+            database_operations=[
+                migrations.RunPython(add_clave_column, migrations.RunPython.noop),
+            ],
+            state_operations=[
+                migrations.AddField(
+                    model_name="medicamentocatalogo",
+                    name="clave",
+                    field=models.CharField(blank=True, max_length=100, null=True),
+                ),
+            ],
         ),
         migrations.RunPython(copiar_codigo, migrations.RunPython.noop),
         migrations.AlterField(
